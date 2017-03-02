f698b7152a15bf89ceb08371fe0dcf2113645901
Softtrends <softtrends@gmail.com>
2017-03-01 22:35:22 -0500

    Initial Commit
    Committing initial changes

---
+++ js/donut-chart-custom.js
@@ -0,0 +1,41 @@
+$('#year-2014').doughnutChart({
+	positiveColor: "#f47b6f",
+	negativeColor: "#f0f0f0",
+	backgroundColor: "white",
+	percentage: 62,
+	size: 150,
+	doughnutSize: 0.35,
+	innerText: "62%",
+	innerTextOffset: 14,
+	Title: "",
+	positiveText: "",
+	negativeText: ""
+});
+
+$('#year-2015').doughnutChart({
+	positiveColor: "#646669",
+	negativeColor: "#f0f0f0",
+	backgroundColor: "white",
+	percentage: 71,
+	size: 150,
+	doughnutSize: 0.35,
+	innerText: "71%",
+	innerTextOffset: 14,
+	Title: "",
+	positiveText: "",
+	negativeText: ""
+});
+
+$('#year-2016').doughnutChart({
+	positiveColor: "#4cc1ef",
+	negativeColor: "#f0f0f0",
+	backgroundColor: "white",
+	percentage: 86,
+	size: 150,
+	doughnutSize: 0.35,
+	innerText: "86%",
+	innerTextOffset: 14,
+	Title: "",
+	positiveText: "",
+	negativeText: ""
+});
